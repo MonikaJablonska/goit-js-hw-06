b5a7ab6d8817277c6df2c010faee0c537620c21c
Monika Jabłońska <monika.pieprzny@gmail.com>
2022-05-17 20:54:37 +0200

    feat(02): solved

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.innerText = ingredient;
+  ingredientsList.append(ingredientItem);
+});
+
+
