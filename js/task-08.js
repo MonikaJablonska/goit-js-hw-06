e40deb65c769cf9d078389926436d59bea08618c
Monika Jabłońska <monika.pieprzny@gmail.com>
2022-07-22 00:19:00 +0200

    feat(08): solved

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (
+    loginForm.elements.email.value === "" ||
+    loginForm.elements.password.value === ""
+  ) {
+    alert("Wszystkie pola powinny być uzupełnione.");
+  } else {
+    const formValues = {
+      email: loginForm.elements.email.value,
+      password: loginForm.elements.password.value,
+    };
+    console.log(formValues);
+    loginForm.reset();
+  }
+});
