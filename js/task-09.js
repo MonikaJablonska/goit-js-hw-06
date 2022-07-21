2c55aba66e41ec6189b6c06eebbb1c5ebe7eeb52
Monika Jabłońska <monika.pieprzny@gmail.com>
2022-07-22 00:44:49 +0200

    feat(09): solved

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const spanColor = document.querySelector(".color");
+const buttonChangeColor = document.querySelector(".change-color");
+buttonChangeColor.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  spanColor.textContent = color;
+});
